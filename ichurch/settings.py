13040aa842ff5c80e5793de2b673fb833e818c53
William Gomes <williamgomes@MacBook-Pro-de-William.local>
2020-01-21 15:50:29 -0400

    deploy python anywhere

--- ichurch/settings.py
+++ ichurch/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'ichurch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,9 +113,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
